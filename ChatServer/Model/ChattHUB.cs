f89c1cee1d761d85d897faac9bca3c79d05fc38b
Fjodor <id19fjs>
2025-05-21 12:45:59 +0200

    Add project files.

---
+++ ChatServer/Model/ChattHUB.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+public class Announcement
+{
+    public string role { get; set; }
+    public string user { get; set; }
+    public string message { get; set; }
+}
+
+public class ChattHUB : Hub
+{
+        private static readonly List<Announcement> Announcements = new();
+        private static readonly Dictionary<string, string> ConnectedUsers = new();
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            if (ConnectedUsers.TryGetValue(Context.ConnectionId, out string userName))
+            {
+                ConnectedUsers.Remove(Context.ConnectionId);
+                await Clients.All.SendAsync("UserLeft", $"{userName} har lämnat chatten.");
+
+                await Clients.All.SendAsync("UpdateUserList", ConnectedUsers.Values.ToList());
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+
+    public async Task JoinChat(string userName, string role)
+    {
+        ConnectedUsers[Context.ConnectionId] = role + " " + userName;
+        await Clients.All.SendAsync("UserJoined", $"{role} {userName} har anslutit till chatten.");
+
+        await Clients.All.SendAsync("UpdateUserList", ConnectedUsers.Values.ToList());
+        BroadcastAnnouncements();
+    }
+    
+
+    public async Task SendMessage(string role, string userName, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", role, userName, message);
+    }
+
+    public async Task SendAnnouncement(string role, string userName, string message)
+    {
+        if (role == "Lärare")
+        {
+            var an = new Announcement { role =role, user = userName, message = message };
+            Announcements.Add(an);
+            BroadcastAnnouncements();
+        }
+    }
+
+   
+    private Task BroadcastAnnouncements()
+    {
+        return Clients.All.SendAsync("ReceiveAnnouncement", Announcements);
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactDevServer",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials();
+        });
+});
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowReactDevServer");
+app.UseAuthorization();
+app.MapHub<ChattHUB>("/chathub");
+
+app.Run();
